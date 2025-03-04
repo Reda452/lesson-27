87a0ebf6c8303e75ce7b755f46bd9ad816b1ecd6
Reda452 <redasakhi0@gmail.com>
2025-03-04 19:56:48 +0000

    new1

---
+++ activity2.py
@@ -0,0 +1,20 @@
+# creat class
+class Employee:
+
+    # Initializing
+    def __init__(self):
+        print('Employee created')
+
+    # calling destructor
+    def __del__(self):
+        print("Destructor called")
+
+def Create_obj():
+    print('Making Object...')
+    obj = Employee()
+    print('function end...')
+    return obj
+
+print('Calling Create_obj() function...')
+obj = Create_obj()
+print('Program End...')
